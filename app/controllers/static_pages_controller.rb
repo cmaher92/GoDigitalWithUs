f967a35c6da46ea10d70e7e95da681b0488326c7
leslie kimm <leslie.kimm@gmail.com>
2016-06-19 18:39:05 -0700

    add simple search

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,11 @@
 class StaticPagesController < ApplicationController
   def home
     @tutorials = Tutorial.all
+    if params[:search]
+      @tutorials = Tutorial.search(params[:search]).order("created_at DESC")
+    else
+      @tutorials = Tutorial.all.order('created_at DESC')
+    end
   end
 
   def contact
