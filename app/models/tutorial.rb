3c9aaf6446dbadbdaf8a430aa89abbce1a76df90
Adam Braus <ajbraus@gmail.com>
2016-07-23 17:56:55 -0700

    added icons and improved tutorial new form

--- app/models/tutorial.rb
+++ app/models/tutorial.rb
@@ -1,4 +1,14 @@
 class Tutorial < ActiveRecord::Base
+
+  TOPICS = [
+    "Internet Basics",
+    "Computer Basics",
+    "Music & Entertainment",
+    "Family & Friends",
+    "Finances & Money",
+    "Government & Licenses"
+  ]
+
   def self.search(search)
     where("name LIKE ?", "%#{search}%") 
     where("content LIKE ?", "%#{search}%")
