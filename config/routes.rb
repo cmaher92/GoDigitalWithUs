4e48b499cd3c712a6f883f8aa6d29bb02c92a40a
leslie kimm <leslie.kimm@gmail.com>
2016-07-26 20:05:25 -0700

    line up left & RHS of hashrocket

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
-  get 'guide' => 'static_pages#guide'
+  root             'static_pages#home'
+  get 'guide'   => 'static_pages#guide'
   get 'contact' => 'static_pages#contact'
 
   resources :tutorials
