90dee2afce448d1bc0b04ceac2282eee42db5c8f
leslie kimm <leslie.kimm@gmail.com>
2016-08-02 20:55:34 -0700

    create developers page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root             'static_pages#home'
   get 'guide'   => 'static_pages#guide'
   get 'contact' => 'static_pages#contact'
+  get 'style'   => 'static_pages#style'
 
   resources :tutorials
 end
