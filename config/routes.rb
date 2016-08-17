d70ad9864bedf3e0b5fb110a994deb73cbaad298
Connor Maher <connormmaher@gmail.com>
2016-08-17 10:36:23 -0700

    Add dynamic error pages with associated controllers and css

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  root               'static_pages#home'\
-  
+  root               'static_pages#home'
   get 'guide'     => 'static_pages#guide'
   get 'contact'   => 'static_pages#contact'
   get 'developer'     => 'static_pages#developer'
+
+  # allows the error pages to be displayed for any type of request
+  match "/404", :to => "errors#not_found", :via => :all
+  match "/500", :to => "errors#internal_server_error", :via => :all
 
   get 'dashboard' => 'users#show'
 
