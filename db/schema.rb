9a0bb18bc113ca46449a12ceb3cdc25020036068
Connor Maher <connormmaher@gmail.com>
2016-08-16 18:31:56 -0700

    Remove steps from new and show views, remove steps partial

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160816235940) do
-
-  create_table "steps", force: :cascade do |t|
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.text     "content"
-    t.integer  "tutorial_id"
-    t.string   "name"
-  end
+ActiveRecord::Schema.define(version: 20160817012546) do
 
   create_table "tutorials", force: :cascade do |t|
     t.string   "title"
